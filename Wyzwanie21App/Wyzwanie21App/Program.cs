100a5aa9181bcd9153ce76a7232d83a1f431945a
Mechanik1975 <chwaszcz.pawel@gmail.com>
2023-11-18 20:38:06 +0100

    dzien 4

--- Wyzwanie21App/Wyzwanie21App/Program.cs
+++ Wyzwanie21App/Wyzwanie21App/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string inName = "Ewa";  // imie
+char inGender = 'w';  // w-kobieta , m-mezczyzna
+int inEage = 33;   // wiek
+
+if(inGender == 'w' && inEage < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if(inGender == 'w' && inName == "Ewa" && inEage == 33)
+{
+    Console.WriteLine("Ewa, lat 33 ");
+}
+else if(inGender != 'w' && inEage < 18)
+{
+    Console.WriteLine("Mężczyzna poniżej 18 roku.");
+}
