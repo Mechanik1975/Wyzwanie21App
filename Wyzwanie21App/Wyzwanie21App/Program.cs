8b05e50712b34750da20f6b2ec4269e8aff01ebe
Mechanik1975 <chwaszcz.pawel@gmail.com>
2023-11-20 22:37:48 +0100

    dzien5

--- Wyzwanie21App/Wyzwanie21App/Program.cs
+++ Wyzwanie21App/Wyzwanie21App/Program.cs
@@ -1,16 +1,72 @@
-﻿string inName = "Ewa";  // imie
-char inGender = 'w';  // w-kobieta , m-mezczyzna
-int inEage = 33;   // wiek
+﻿var number = 568125546568425;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToCharArray();
 
-if(inGender == 'w' && inEage < 30)
+var counter0 = 0;
+var counter1 = 0;
+var counter2 = 0;
+var counter3 = 0;
+var counter4 = 0;
+var counter5 = 0;
+var counter6 = 0;
+var counter7 = 0;
+var counter8 = 0;   
+var counter9 = 0;
+
+foreach (var letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
+
 }
-else if(inGender == 'w' && inName == "Ewa" && inEage == 33)
-{
-    Console.WriteLine("Ewa, lat 33 ");
-}
-else if(inGender != 'w' && inEage < 18)
-{
-    Console.WriteLine("Mężczyzna poniżej 18 roku.");
-}
+Console.WriteLine("LICZBA  " + number);
+Console.WriteLine(" 0=> " + counter0);
+Console.WriteLine(" 1=> " + counter1);
+Console.WriteLine(" 2=> " + counter2);
+Console.WriteLine(" 3=> " + counter3);
+Console.WriteLine(" 4=> " + counter4);
+Console.WriteLine(" 5=> " + counter5);
+Console.WriteLine(" 6=> " + counter6);
+Console.WriteLine(" 7=> " + counter7);
+Console.WriteLine(" 8=> " + counter8);
+Console.WriteLine(" 9=> " + counter9);
+
+
